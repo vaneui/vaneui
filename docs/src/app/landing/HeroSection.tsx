7e5874aca6cfde87b86868b525973de318d07bf7
Evgeniy Kosjakov <elv1s42k@gmail.com>
2025-03-26 19:29:26 +0200

    Update HeroSection.tsx

--- docs/src/app/landing/HeroSection.tsx
+++ docs/src/app/landing/HeroSection.tsx
@@ -10,12 +10,12 @@ import {
   Card,
   Stack,
   Chip,
-  Divider
+  Divider,
+  Badge
 } from 'vaneui';
-import { PRODUCT } from '../constants';
-import { CodeBlock } from "../components/CodeBlock";
+import {PRODUCT} from '../constants';
+import {CodeBlock} from "../components/CodeBlock";
 import Link from "next/link";
-import {Badge} from "../../../../src";
 
 const dog = {
   name: "Luka",
@@ -26,18 +26,21 @@ const dog = {
 
 export function HeroSection() {
   return (
-    <Section relative className="pt-20 gap-16 border-b bg-gradient-to-r from-slate-100 via-white to-slate-100 mt-[calc(36px+(var(--spacing)*6))]">
-      <Row absolute className="inset-0 bg-[radial-gradient(var(--color-slate-200)_1px,transparent_1px)] [background-size:calc(var(--spacing)*4)_calc(var(--spacing)*4)]" />
+    <Section relative
+             className="pt-20 gap-16 border-b bg-gradient-to-r from-slate-100 via-white to-slate-100 mt-[calc(36px+(var(--spacing)*6))]">
+      <Row absolute
+           className="inset-0 bg-[radial-gradient(var(--color-slate-200)_1px,transparent_1px)] [background-size:calc(var(--spacing)*4)_calc(var(--spacing)*4)]"/>
       <Container xs className="z-10">
         <Col xl itemsCenter>
-          <Badge normalCase normal>Open source components</Badge>
+          <Badge normalCase light>Open source components</Badge>
           <PageTitle xl sans textCenter medium>
             {PRODUCT.slogan}
           </PageTitle>
           <Text lg textCenter>{PRODUCT.description}</Text>
           <Row lg smCol justifyCenter className="w-full">
             <Button lg filled className="max-sm:w-full">Get Started</Button>
-            <Button lg secondary className="max-sm:w-full" target="_blank" href={PRODUCT.githubUrl} tag={Link}>View on GitHub</Button>
+            <Button lg secondary className="max-sm:w-full" target="_blank" href={PRODUCT.githubUrl} tag={Link}>View on
+              GitHub</Button>
           </Row>
         </Col>
       </Container>
@@ -46,13 +49,13 @@ export function HeroSection() {
           <Col className="max-w-xl max-sm:max-w-80 z-20 border-8 rounded-3xl border-gray-400/10 backdrop-blur-sm">
             <Card xl noPadding>
               <Row noGap smCol>
-                <img src={dog.image} alt="puppy" className="self-stretch w-52 max-sm:w-80" />
+                <img src={dog.image} alt="puppy" className="self-stretch w-52 max-sm:w-80"/>
                 <Stack>
                   <Row justifyBetween>
                     <Title>{dog.name}</Title>
                     <Chip sm bold>{dog.gender}</Chip>
                   </Row>
-                  <Divider />
+                  <Divider/>
                   <Text sm>{dog.description}</Text>
                   <Row justifyEnd>
                     <Button sm success filled>Adopt</Button>
@@ -63,9 +66,9 @@ export function HeroSection() {
             </Card>
           </Col>
           <CodeBlock className="z-0 lg:-mt-[calc(var(--spacing)*20)] shadow-xl"
-            fileName="DogCard.tsx"
-            language="tsx"
-            code={`import { Card, Row, Stack, Title, Text, Chip, Divider, Button } from '@vaneui/ui';
+                     fileName="DogCard.tsx"
+                     language="tsx"
+                     code={`import { Card, Row, Stack, Title, Text, Chip, Divider, Button } from '@vaneui/ui';
 import dog from './data/dog.json';
 
 export function DogCard() {
