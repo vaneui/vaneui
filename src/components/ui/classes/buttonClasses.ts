a0ef18cfed22b2ad9904cca6492c876e07863340
Evgeniy Kosjakov <elv1s42k@gmail.com>
2025-04-02 17:59:22 +0300

    Update buttonClasses.ts

--- src/components/ui/classes/buttonClasses.ts
+++ src/components/ui/classes/buttonClasses.ts
@@ -10,7 +10,6 @@ import {
   hoverBackgroundAppearanceClasses
 } from "./appearanceClasses";
 import { filledTextAppearanceClasses, textAppearanceClasses } from "./typographyClasses";
-import { commonGaps } from "./spacingClasses";
 
 export class ButtonBaseClasses {
   background: Record<keyof CommonAppearanceProps, string>;
@@ -34,7 +33,7 @@ export class ButtonBaseClasses {
   shadow: Record<keyof SizeProps, string> = shadowClasses;
   px: Record<keyof SizeProps, string> = {xs: "px-2", sm: "px-2.5", md: "px-3.5", lg: "px-5", xl: "px-6"};
   py: Record<keyof SizeProps, string> = {xs: "py-1", sm: "py-1.5", md: "py-2", lg: "py-3", xl: "py-4"};
-  gap: Record<keyof SizeProps, string> = commonGaps;
+  gap: Record<keyof SizeProps, string> = {xs: "gap-1.5", sm: "gap-2", md: "gap-3", lg: "gap-4", xl: "gap-5"};
 
   constructor(
     bg: Record<keyof CommonAppearanceProps, string> = backgroundAppearanceClasses,
