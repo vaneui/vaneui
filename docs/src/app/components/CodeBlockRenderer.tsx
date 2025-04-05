3b5a35edea67378323ebef01d87ab2b9a023a0dd
Evgeniy Kosjakov <elv1s42k@gmail.com>
2025-04-05 17:20:16 +0300

    Revert "fixes"
    This reverts commit ce898e39d2ab2dd35c10d63c47da9a90f5a890eb.

--- docs/src/app/components/CodeBlockRenderer.tsx
+++ docs/src/app/components/CodeBlockRenderer.tsx
@@ -1,7 +1,7 @@
-"use client";
+'use server';
 
-import React, { useEffect, useState } from 'react';
-import { highlight } from './client-syntax-highlighter';
+import React from 'react';
+import { highlight } from './syntax-highlighter';
 import { Row, Col } from '@vaneui/ui';
 
 interface CodeBlockRendererProps {
@@ -9,55 +9,39 @@ interface CodeBlockRendererProps {
   language: string;
 }
 
-export function CodeBlockRenderer({ code, language }: CodeBlockRendererProps) {
-  const [highlightedCode, setHighlightedCode] = useState<string>('');
-  const [highlightedLineNumbers, setHighlightedLineNumbers] = useState<string>('');
-  const [isClient, setIsClient] = useState(false);
+export async function CodeBlockRenderer({ code, language }: CodeBlockRendererProps) {
+  // First perform syntax highlighting on the original code
+  const highlightedCode = highlight(code, language);
 
-  useEffect(() => {
-    setIsClient(true);
+  // Split the code to count lines for rendering
+  const codeLines = code.split('\n');
 
-    // Perform syntax highlighting on the client side
-    const highlighted = highlight(code, language);
-    setHighlightedCode(highlighted);
-
-    // Split the code to count lines for rendering
-    const codeLines = code.split('\n');
-
-    // Generate line numbers
-    const lineNumbers = codeLines.map((_, i) => i + 1).join('\n');
-    const highlightedLines = highlight(lineNumbers, language);
-    setHighlightedLineNumbers(highlightedLines);
-  }, [code, language]);
+  // Generate line numbers
+  const lineNumbers = codeLines.map((_, i) => i + 1).join('\n');
+  const highlightedLineNumbers = highlight(lineNumbers, language);
 
   return (
     <Row noGap className="overflow-auto">
-      {isClient ? (
-        <>
-          {/* Line numbers column */}
-          <Col>
-            <pre className="m-0 py-4 pl-6 pr-2 overflow-visible">
-              <code
-                className={`language-${language} font-mono text-sm`}
-                dangerouslySetInnerHTML={{
-                  __html: highlightedLineNumbers
-                }}
-              />
-            </pre>
-          </Col>
-          {/* Code with syntax highlighting */}
-          <Col className="flex-1">
-            <pre className="m-0 py-4 pl-2 pr-6 overflow-visible">
-              <code
-                className={`language-${language} font-mono text-sm`}
-                dangerouslySetInnerHTML={{ __html: highlightedCode }}
-              />
-            </pre>
-          </Col>
-        </>
-      ) : (
-        <div>Loading code...</div>
-      )}
+      {/* Line numbers column */}
+      <Col>
+        <pre className="m-0 py-4 pl-6 pr-2 overflow-visible">
+          <code
+            className={`language-${language} font-mono text-sm`}
+            dangerouslySetInnerHTML={{
+              __html: highlightedLineNumbers
+            }}
+          />
+        </pre>
+      </Col>
+      {/* Code with syntax highlighting */}
+      <Col className="flex-1">
+        <pre className="m-0 py-4 pl-2 pr-6 overflow-visible">
+          <code
+            className={`language-${language} font-mono text-sm`}
+            dangerouslySetInnerHTML={{ __html: highlightedCode }}
+          />
+        </pre>
+      </Col>
     </Row>
   );
-}
+} 
