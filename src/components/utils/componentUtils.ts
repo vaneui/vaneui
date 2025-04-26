06e2f254f4325b77471a348609e0a9b3a00c2c8c
Evgeniy Kosjakov <elv1s42k@gmail.com>
2025-04-26 18:07:11 +0300

    Update componentUtils.ts

--- src/components/utils/componentUtils.ts
+++ src/components/utils/componentUtils.ts
@@ -1,17 +1,3 @@
-export const getFirstTruthyKey = <T extends object>(
-  defaultValue: keyof T,
-  props: T
-): keyof T => {
-  const keys = Object.keys(props) as (keyof T)[];
-  const activeKeys = keys.filter(key => (props[key] as unknown as boolean));
-  if (activeKeys.length > 1) {
-    console.warn(
-      `Multiple keys are active (${activeKeys.join(", ")}). Using the first one.`
-    );
-  }
-  return activeKeys[0] || defaultValue;
-};
-
 export function pickFirst<
   P,
   K extends keyof P
